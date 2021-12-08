e2b1474513a539f06aac0b598aaa1d27116b382d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-08 20:04:29 +0530

    kb8.c

---
+++ KB8.C
@@ -0,0 +1,22 @@
+main()
+{
+     int r,c;
+     char a='a';
+     clrscr();
+     for(r=1;r<=5;r++)
+     {
+	for(c=1;c<=5;c++)
+	{
+	   if(r%2==0)
+	   {
+	      printf("a");
+	   }
+	   else
+	   {
+	      printf("%d",c);
+	   }
+	}
+	printf("\n");
+     }
+     getch();
+}
