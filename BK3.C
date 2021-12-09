d6643d7c36a4f926abaa99a94c88fe337766b4fc
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-09 14:59:49 +0530

    Add files via upload

---
+++ BK3.C
@@ -0,0 +1,19 @@
+/* 12345
+   12345
+   12345
+   12345
+*/
+main()
+{
+     int r,c;
+     clrscr();
+     for(r=1;r<=4;r++)
+     {
+	 for(c=1;c<=5;c++)
+	 {
+	     printf("%d",c);
+	 }
+	 printf("\n");
+     }
+     getch();
+}
