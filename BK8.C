a9f532fa862e06796fe764c9775965a5ee7167a0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-09 15:57:10 +0530

    Add files via upload

---
+++ BK8.C
@@ -0,0 +1,19 @@
+/* EDCBA
+   EDCBA
+   EDCBA
+   EDCBA
+*/
+main()
+{
+     int r,c;
+     clrscr();
+     for(r='D';r>='A';r--)
+     {
+	 for(c='E';c>='A';c--)
+	 {
+	     printf("%c",c);
+	 }
+	 printf("\n");
+     }
+     getch();
+}
