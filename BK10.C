d3249d2a81cd4a81129b02fbc2fde0c9c0f2bdd4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-09 18:21:20 +0530

    Add files via upload

---
+++ BK10.C
@@ -0,0 +1,20 @@
+/* print * * * * *
+	 * * * *
+	 * * *
+	 * *
+	 *
+*/
+main()
+{
+     int r,c;
+     clrscr();
+     for(r=5;r>=1;r--)
+     {
+	 for(c=1;c<=r;c++)
+	 {
+	    printf("*");
+	 }
+	 printf("\n");
+     }
+     getch();
+}
