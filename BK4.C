e2bba45f1501ef40dc45b3926d8d3095c2147363
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-09 15:00:08 +0530

    Add files via upload

---
+++ BK4.C
@@ -0,0 +1,19 @@
+/* AAAAA
+   BBBBB
+   CCCCC
+   DDDDD
+*/
+main()
+{
+     int r,c;
+     clrscr();
+     for(r='A';r<='D';r++)
+     {
+	 for(c='A';c<='E';c++)
+	 {
+	     printf("%c",r);
+	 }
+	 printf("\n");
+     }
+     getch();
+}
