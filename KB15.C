5de32053bc9e774395e9a5ed447616cacfcfd330
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-09 13:15:14 +0530

    Add files via upload

---
+++ KB15.C
@@ -0,0 +1,11 @@
+/*Write  c program to calculate  the distance between two points*/
+main()
+{
+     float dis,p1,p2;
+     clrscr();
+     printf("Enter the value p1,p2");
+     scanf("%f %f",&p1,&p2);
+     dis=(p2-p1);
+     printf("Distance=%f",dis);
+     getch();
+}
