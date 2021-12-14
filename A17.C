4064d350783a0fb1d1c5ec0fe4641f3c80715899
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-14 18:58:22 +0530

    Add files via upload

---
+++ A17.C
@@ -0,0 +1,12 @@
+/*wap to create char arry to store  character in array*/
+main()
+{
+     int i;
+     char name[5]={'k','e','t','a','n'};
+     clrscr();
+     for(i=0;i<5;i++)
+     {
+	  printf("%c",name[i]);
+     }
+     getch();
+}
