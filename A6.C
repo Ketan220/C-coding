4954dc68b51b4461d5d213c841251e5c2ee1d727
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-14 13:19:10 +0530

    Add files via upload

---
+++ A6.C
@@ -0,0 +1,12 @@
+/* Creating array to store 5 subject marks*/
+main()
+{
+     int marks[5]={45, 67,78,65,34};
+     int i;
+     clrscr();
+     for(i=0;i<5;i++)
+     {
+	printf("\nmarks of 5 subject =%d",marks[i]);
+     }
+     getch();
+}
