65511eeaeb61afcafc890b4edc5df29f710117b4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-14 13:19:38 +0530

    Add files via upload

---
+++ A8.C
@@ -0,0 +1,23 @@
+/*Program tp search an element in an array*/
+main()
+{
+     int arr[5]={1,2,3,4,5};
+     int i,num,
+     clrscr();
+     printf("Enter the number");
+     scanf("%d",&num);
+     for(i=1;i<5;i++)
+     {
+	  if(num=arr[5])
+	  {
+	     printf("No. is found=%d",num);
+	     break;
+	  }
+     }
+     if(i=5)
+     {
+     printf("No. is not found");
+     }
+     getch();
+}
+
