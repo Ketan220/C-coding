71534d59dcb95728a219a82eac9e87690891d004
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-16 15:49:12 +0530

    Add files via upload

---
+++ F2.C
@@ -0,0 +1,21 @@
+/* Function without parameter and  not returning value
+ex1. Creat a  function  named addition to accept 2 noand diplays the
+addition of 2 no. */
+#include<stdio.h>
+addition()
+{
+     int n1,n2,res;
+     clrscr();
+     printf("\nEnter th two no.");
+     scanf("%d %d",&n1,&n2);
+     res=n1+n2;
+     printf("\naddition=%d",res);
+     getch();
+}
+
+main()
+{
+	printf("\n calling addition from main function");
+	addition();
+	printf("\n");
+}
