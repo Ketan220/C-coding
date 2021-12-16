5556285e8ba72ec4845e2e153f24ebf656b442f6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-16 15:52:16 +0530

    Add files via upload

---
+++ F11.C
@@ -0,0 +1,23 @@
+/*  Function without parameter and not returning value
+1-creat a function factorial with one in parameter  to accept no. to
+calculate  and diplay the factorial of the program*/
+main()
+{
+     int r,num;
+     clrscr();
+     printf("Enter the no");
+     scanf("%d",&num);
+     r=fact(num);
+     printf("factorial=%d",r);
+     getch();
+}
+
+fact(int x)
+{
+      int i,fact=1;
+      for(i=1;i<=x;i++)
+      {
+	   fact=fact*i;
+      }
+
+}
