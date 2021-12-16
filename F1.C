e4f6f77dec49a69b954b4a5bd2d51cbe49957980
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-16 15:48:46 +0530

    Add files via upload

---
+++ F1.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void display()
+{
+	 printf("\n hello ketan");
+
+		    printf("\n hello ketan kumar");
+}
+
+void main()
+{
+	display();
+		 printf("\n hello every body");
+		     display();
+			 printf("\n hello mkpit");
+			     display();
+				 printf("\nhi");
+				       display();
+}
+
