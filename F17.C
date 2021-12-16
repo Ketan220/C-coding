8649d077875e8529ff4d1cc377850da5f690384c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-16 15:55:16 +0530

    Add files via upload

---
+++ F17.C
@@ -0,0 +1,20 @@
+/* ex1 of Function*/
+main()
+{
+      clrscr();
+      printf("\nHAPPY NEW YEAR");
+      line();
+      printf("\nhappy \nnew \nyear");
+      line();
+      getch();
+}
+
+line()
+{
+      int i;
+      printf("\n");
+      for(i=1;i<=10;i++)
+      {
+	  printf("--");
+      }
+}
