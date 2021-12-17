23c8dd49e84d729f4059fc380024d283e3c8874a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-17 15:24:24 +0530

    Add files via upload

---
+++ S4.C
@@ -0,0 +1,12 @@
+/* Reads & writes string in c using gets and puts function*/
+#include<string.h>
+main()
+{
+     char name[20];
+     clrscr();
+     puts("Enter you name");
+     gets(name);
+     puts("name is"  );
+     puts(name);
+     getch();
+}
