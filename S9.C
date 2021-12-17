bbd1a8274cc37ed14147f3b46445693b9a719dab
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-17 15:26:17 +0530

    Add files via upload

---
+++ S9.C
@@ -0,0 +1,18 @@
+/* ex2 c star function-strcmp */
+main()
+{
+     char name[10]="ketan";
+     char name1[10]="ketanb";
+     int res;
+     clrscr();
+     res=strcmp(name,name1);
+     if(res==0)
+     {
+	     printf("Strings are equal");
+     }
+     else
+     {
+	     printf("Strings are not equal");
+     }
+     getch();
+}
