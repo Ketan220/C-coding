0cc78381fec61829b9ba300ecd3e1dbba1c28761
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-17 15:24:06 +0530

    Add files via upload

---
+++ S3.C
@@ -0,0 +1,10 @@
+/* Declaring char array to store name*/
+main()
+{
+     char name[10];
+     clrscr();
+     printf("Enter your name");
+     scanf("%s",&name);
+     printf("%s",name);
+     getch();
+}
