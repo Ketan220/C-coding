41034d3b4e004b2e784b424219a1cdee8891a8d4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-17 15:23:45 +0530

    Add files via upload

---
+++ S2.C
@@ -0,0 +1,12 @@
+/* Declaring char array to store name*/
+main()
+{
+     char name[10]={"Mohnati"};
+     int i;
+     clrscr();
+     for(i=0;i<7;i++)
+     {
+	printf("%c",name[i]);
+     }
+     getch();
+}
