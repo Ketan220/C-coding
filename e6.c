9d012fe8b7c8040f207d2f402ac20f23e5fa28b9
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-23 22:05:38 +0530

    Add files via upload

---
+++ e6.c
@@ -0,0 +1,12 @@
+//ex6//
+#include<stdio.h>
+enum months{jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec};
+int main()
+{
+    system("cls");
+    for(int i=jan;i<=dec;i++);
+    {
+        printf("\n%d");
+    }
+    return 0;
+}
