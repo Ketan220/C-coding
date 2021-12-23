b3b722ae5daede0d1e917ce3fd8e257aef28b984
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-23 22:05:52 +0530

    Add files via upload

---
+++ e7.c
@@ -0,0 +1,9 @@
+//Two enum having same scope but then these two enums should have different enum names otherwise complier will throw an error//
+#include<stdio.h>
+enum status{sucess,fail};
+enum boolean{fail,pass};
+void main()
+{
+    system("cls");
+    printf("The value of sucess is=%d",sucess);
+}
