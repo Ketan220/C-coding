83c3e8b9f203f1753d36585917f5ce78e7d0012d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-23 22:05:24 +0530

    Add files via upload

---
+++ e5.c
@@ -0,0 +1,11 @@
+//ex5//
+#include<stdio.h>
+enum weekdays{monday,tuesdsy,wedneday,thursday,friday,saturday,sunday};
+main()
+{
+    enum weekdays w;
+    w=thursday;
+    system("cls");
+    printf("The value of thursday=%d",w);
+
+}
