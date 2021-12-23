2e23892302698de67321396a87fa1db62abe2ded
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-23 22:04:53 +0530

    Add files via upload

---
+++ e3.c
@@ -0,0 +1,10 @@
+//ex3//
+#include<stdio.h>
+enum fruits{Apple=45,Mangoes=65,Anar=67,Angur=45};
+main()
+{
+   enum fruits f;
+   f=Mangoes;
+   system("cls");
+   printf("The quantity of Mangoes =%d",f);
+}
