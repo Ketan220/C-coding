cd3c48f48aebf1420a9755cca7e7d2da0d7d13bb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2021-12-24 12:38:12 +0530

    Add files via upload

---
+++ r8.c
@@ -0,0 +1,26 @@
+//Below the program to find largest and smallest value using global declaration//
+#include<stdio.h>
+int a,b;
+main()
+{
+    printf("\n\nEnter the two values to find the greatest and smallest number\n");
+    scanf("%d %d",&a,&b);
+    if(a==b)
+    
+        printf("Both are equal\n");
+    
+    else if(a<b)
+    {
+        printf("\n\nThe largest no =%03d\n",b);
+        printf("\n\nThe Smallest no =%03d\n",a);
+        printf("\n\nThe largest no =%03d\n",b);
+
+    }
+       else
+    aining
+    {
+        printf("\n\nThe largest no =%03d\n",b);
+        printf("\n\nThe Smallest no =%03d\n",a);
+        return 0;   
+    }
+}
